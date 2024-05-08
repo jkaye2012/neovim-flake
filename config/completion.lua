b9d08da87247f0cd7ad8ea38e73d66e3e6d90eb8
Jordan Kaye <jordan.kaye2@gmail.com>
2024-05-07 20:48:50 -0600

    Disables markdown autocompletion

--- config/completion.lua
+++ config/completion.lua
@@ -18,7 +18,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-l>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = cmp.mapping.select_next_item({behavior=cmp.SelectBehavior.Insert}),
@@ -42,6 +42,10 @@ cmp.setup.filetype('gitcommit', {
   }, {
     { name = 'buffer' },
   })
+})
+
+cmp.setup.filetype('markdown', {
+  completion = { autocomplete = false }
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
